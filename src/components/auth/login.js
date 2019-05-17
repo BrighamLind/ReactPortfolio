1dd3ec347c7eb7e978a9960036fc61ffc094555d
BrighamLind <brighamlind@gmail.com>
2019-05-16 18:59:59 -0600

    Finished login authentication and began work on the Portfolio Manager

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,7 +7,8 @@ export default class Login extends Component {
 
         this.state = {
             email: "",
-            password: ""
+            password: "",
+            errorText: ""
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -16,7 +17,8 @@ export default class Login extends Component {
 
     handleChange(event) {
         this.setState({
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
+            errorText: ""
         })
     }
 
@@ -32,7 +34,20 @@ export default class Login extends Component {
                 withCredentials: true
             }
         ).then(response => {
-            console.log("response", response);
+            if (response.data.status === "created") {
+                this.props.handleSuccessfulAuth();
+            } else {
+                this.setState({
+                    errorText: "Wrong email or password"
+                })
+                this.props.handleUnsuccessfulAuth();
+            }
+        })
+        .catch(error => {
+            this.setState({
+                errorText: "An error occurred"
+            })
+            this.props.handleUnsuccessfulAuth();
         });
         
         event.preventDefault();
@@ -42,6 +57,8 @@ export default class Login extends Component {
         return (
             <div>
                 <h1>Login to access your dashboard</h1>
+
+                <p>{this.state.errorText}</p>
 
                 <form onSubmit={this.handleSubmit}>
                     <input
